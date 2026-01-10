147ea40a8b833c914026369c51546ed2e2e9bdaa
ALI <a711879040@gmail.com>
2026-01-10 18:36:00 +0300

    Add LogoutController to handle user logout requests

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        // Logout the user
+        $request->user()->currentAccessToken()->delete();
+        
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
