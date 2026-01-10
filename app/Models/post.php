52bfe8676df290c4d5601a3e89a41f174a581289
ALI <a711879040@gmail.com>
2026-01-10 23:10:12 +0300

    Add Post model with relationships to User, Comment, and likes

---
+++ app/Models/post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Dom\Comment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory ;
+
+    protected $fillable = [
+        'content' ,
+        'user_id',
+    ];
+
+    // Relationships :-
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function likes() {
+        return $this->belongsToMany(User::class , 'likes')->withTimestamps();
+    }
+    
+}
