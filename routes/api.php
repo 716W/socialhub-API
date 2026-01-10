11e90e039dcc17bcd29db8f365311650271bc12e
ALI <a711879040@gmail.com>
2026-01-11 01:10:35 +0300

    Add API resource routes for managing posts

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,8 @@ Route::post('/login',LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout',LogoutController::class);
+
+    Route::apiResource('posts', PostController::class);
 
     // for test the current user just :-
     Route::get('/user', fn (Request $request) => $request->user());
