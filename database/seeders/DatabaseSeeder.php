0368b19bbed3c1db817a3bb16de2ee86360b85d1
ALI <a711879040@gmail.com>
2026-01-11 22:50:34 +0300

    add PostFactory & UserFactory with Admin User to test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,31 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $user = User::factory(10)->create();
 
+        // Randomly create posts for the users :-
+        Post::factory(10)
+            ->recycle($user)
+            ->create();
+
+        // Create an admin user :-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin',
         ]);
     }
 }
