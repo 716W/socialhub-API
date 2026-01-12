1c66619f4252ecda333ec67349563265beed3c63
ALI <a711879040@gmail.com>
2026-01-12 11:42:16 +0300

    add CommentService to handle comment-related operations

---
+++ app/Services/CommentService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Comment ;
+use App\Models\Post;
+
+class CommentService
+{
+    public function getCommentById(int $commentId)
+    {
+        return Comment::with('user')
+                    ->findOrFail($commentId);
+    }
+
+    public function getCommentsForPost(int $postId)
+    {
+        return Comment::where('post_id' , $postId)
+                ->with('user')
+                ->latest()
+                ->get();
+    }
+    public function createComment(string $content , int $userId , int $postId)
+    {
+        return Comment::create([
+            'content' => $content ,
+            'user_id'=> $userId,
+            'post_id'=> $postId,
+        ]);
+    }
+
+    public function updateComment(int $commentId , array $arrData)
+    {
+        $comment = Comment::findOrFail($commentId);
+        $comment->update([
+            'content'=> $arrData['content'],
+        ]);
+        return $comment->fresh();
+    }
+
+    public function deleteComment(int $commentId)
+    {
+        return (bool) Comment::destroy($commentId);
+    }
+}
