94392df1d3daf9e639ad2b84e60621938563bac4
Ali Bin-Samida <a711879040@gmail.com>
2026-02-04 23:03:33 +0300

    feat: create UserProfile model with user relationship and fillable attributes

---
+++ app/Models/UserProfile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    protected $table = "user_profiles";
+    protected $fillable = [
+        'user_id',
+        'username',
+        'bio',
+        'avatar',
+        'website',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
