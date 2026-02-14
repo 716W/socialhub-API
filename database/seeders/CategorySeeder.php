c7551f9dd49f0c5540c4cb246abc4761ba883d85
Ali Bin-Samida <a711879040@gmail.com>
2026-02-14 14:32:38 +0300

    feat: add seeders for Categories and Tags

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Technology', 'slug' => 'technology'],
+            ['name' => 'Health', 'slug' => 'health'],
+            ['name' => 'Travel', 'slug' => 'travel'],
+            ['name' => 'Food', 'slug' => 'food'],
+            ['name' => 'Education', 'slug' => 'education'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+    }
+}
